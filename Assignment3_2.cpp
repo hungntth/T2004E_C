58d5eeab024e7492b22f9d10a85b93b835d3bdac
hungntth <64962264+hungntth@users.noreply.github.com>
2020-05-14 10:44:16 +0700

    up

---
+++ Assignment3_2.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+	int a=4000;
+	int n1=a+a*8/100;
+	int n2=n1+n1*8/100;
+	int n3=n2+n2*8/100;
+	int n4=n3+n3*8/100;
+	printf("%d",n4);
+	
+}
---
+++ Assignment4_3.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main (){
+	int a = 0;
+	int a2 = 0;
+	for ( ;a<1000;a+1){
+		a2=a2+a+1;
+		a+=1;
+
+	}
+	printf("%d",a2);
+}
